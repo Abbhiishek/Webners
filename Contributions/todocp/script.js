9ff7a46a9257161bbee07b42bc48ed8999bb9f1b
Chandan Pandey <chandan.dv682@gmail.com>
2022-10-10 19:05:18 +0530

    Added a Task history panel | Tasks are now getting stored in the sites localStorage (#45)
    * nop
    
    * Tasks are getting stored in the sites local. But needs some more work
    
    * Added a task history panel, Modified application interface

--- Contributions/todocp/script.js
+++ Contributions/todocp/script.js
@@ -1,24 +1,100 @@
+//Get all the elements
 const tasklist = document.getElementById('tasklist')
+const savedtasks = document.getElementById('localtasks')
 const taskname = document.getElementById('taskname')
 const addtask = document.getElementById('addtask')
 const cleartask = document.getElementById('cleartask')
+const delbtn = document.getElementById('del')
+const backbtn = document.getElementById('back')
+var tc = 1
 
-function addtotasks(){
-  const li = document.createElement('li')
-  const task = document.createTextNode(taskname.value)
-  li.appendChild(task)
-  tasklist.appendChild(li)
+
+//Function to Fetch local storage data
+function load(){
+  // console.log(localStorage.length)
+  while(tc != localStorage.length){
+    var taskvals = localStorage.getItem(tc)
+    const li2 = document.createElement('li')
+    const taskn = document.createTextNode(taskvals)
+    li2.appendChild(taskn)
+    savedtasks.appendChild(li2)
+    tc++
+  }
 }
+
+//Function to add task in the list
+function addtotasks(e){
+  if(e.key === "Enter"){
+      const msg = document.getElementById('msg')
+      const li = document.createElement('li')
+      const task = document.createTextNode(taskname.value)
+      msg.style.display = "block";
+      
+      li.appendChild(task)
+      tasklist.appendChild(li)
+
+      localStorage.setItem(tc,li.innerText)
+      tc++
+  }
+
+}
+function addtotask(){
+      const msg = document.getElementById('msg')
+      const li = document.createElement('li')
+      const task = document.createTextNode(taskname.value)
+      msg.style.display = "block";
+      
+      li.appendChild(task)
+      tasklist.appendChild(li)
+      // savedtasks.appendChild(li)
+
+      localStorage.setItem(tc,li.innerText)
+      var taskvals = localStorage.getItem(tc)
+
+      const li2 = document.createElement('li')
+      const taskn = document.createTextNode(taskvals)
+      li2.appendChild(taskn)
+      savedtasks.appendChild(li2)
+
+      tc++
+}
+//Clear the tasklist
 function cleartasks(){
-   tasklist.innerHTML=''
+  tasklist.innerHTML=''
+  msg.style.display='none'
 }
-addtask.addEventListener('click',addtotasks)
-cleartask.addEventListener('click',cleartasks)
+//Delete all the tasks from storage
+function delall(){
+  savedtasks.innerHTML=''
+  localStorage.clear();
+  tc = 1
+}
+//Take a backup
+// function backup(){
+//   var data = {
+//     task : "Go to the gym"
+//   }
+//   var datastring = JSON.stringify(data)
+//   var fs = require('fs')
+//   fs.writeFile("tasks.json",datastring,function(err, result){
+//     if(err) console.log('error',err);
+//   })
+// }
 
-var list = document.querySelector('ul')
-list.addEventListener('click',function(ev){
+//Callers
+addtask.addEventListener('click',addtotask)
+cleartask.addEventListener('click',cleartasks)
+delbtn.addEventListener('click',delall)
+backbtn.addEventListener('click',load)
+taskname.addEventListener('keypress',addtotasks)
+
+
+tasklist.addEventListener('click',function(ev){
   if(ev.target.tagName==='LI'){
     ev.target.classList.toggle('checked');
-  }
+
+ }
 },false)
+
+
 
