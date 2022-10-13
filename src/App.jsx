df472ef650dd38ee38ab4e18cd43dad62d41b896
Beluga <92670558+beluga2003@users.noreply.github.com>
2022-10-13 18:20:58 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
 import './App.css'
+import ContributorCard from './components/ContributorCard'
+import React from 'react'
 
 function App() {
+  const [contributors, setContributors] = React.useState([])
+  React.useEffect(() => {
+    fetch('./../CONTRIBUTORS.json')
+
+      .then(res => res.json())
+      .then(data => {
+        setContributors(data)
+      })
+  }, [])
+
 
 
   return (
-    <h1 className='text-center text-xl'>Coming Soon ..</h1>
+    <>
+      <div className='py-10 bg-slate-900'>
+        <h1 className='text-4xl text-center font-mono text-white '>Contributors</h1>
+
+        <div className='grid lg:grid-cols-4 md:grid-cols-2 grid-col-1 gap-3 container mx-auto p-10'>
+          {
+            contributors.map((contributor) => (
+              <ContributorCard contributor={contributor} key={contributor.id} />
+            ))
+          }
+        </div>
+
+      </div>
+    </>
   )
 }
 
